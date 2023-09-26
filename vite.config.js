f5c051d9e9dbd3f369d7463d8189d833f3072d12
unknown <ashm.maurya@outlook.com>
2023-09-26 23:18:45 +0530

    first2 commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import { join } from 'path';
+import react from "@vitejs/plugin-react";
 
+// export default defineConfig({
+//   root: join(process.cwd(), 'src'),
+//   server: {
+//     port: 3000,
+//     open: '/index.html',
+//   },
+//   preview: {
+//     port: 8080,
+//   },
+//   build: {
+//     outDir: join(process.cwd(), 'dist'),
+//   },
+// });
 export default defineConfig({
-  root: join(process.cwd(), 'src'),
-  server: {
-    port: 3000,
-    open: '/index.html',
-  },
-  preview: {
-    port: 8080,
-  },
-  build: {
-    outDir: join(process.cwd(), 'dist'),
-  },
+  base: "/unsplash/",
+  plugins: [react()],
 });
